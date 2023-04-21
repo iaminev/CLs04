b1b79d02a1bb16e21550b58e2d31de31a6b3e3ad
Ilshat Aminev <i.aminev@gmail.com>
2023-04-21 08:46:19 +0300

    Task 29

---
+++ Task 29/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33-> [6, 1, 33]*/
+
+int[] InputArray()
+{
+    string digits = "0123456789";
+    System.Console.Write("Input a bunch of numbers: ");
+    string arrayAsString = System.Console.ReadLine()!;
+
+    List<int> numbers = new List<int>();
+
+    string currentNumber = "";
+    int numberAsNumber;
+    for (int i = 0; i < arrayAsString.Length; i++)
+    {
+        int indexOfChar = digits.IndexOf(arrayAsString[i]);
+        //System.Console.WriteLine("" + arrayAsString[i] + " - " + indexOfChar);
+
+        if (indexOfChar >= 0)
+        {
+            currentNumber += arrayAsString[i];
+        }
+        else
+        {
+            if (currentNumber.Length > 0)
+            {
+                bool itIsANumber = int.TryParse(currentNumber, out numberAsNumber);
+                numbers.Add(numberAsNumber);
+                currentNumber = "";
+            }
+        }
+    }
+    if (currentNumber.Length > 0) // pick up the last number
+    {
+        bool itIsANumber = int.TryParse(currentNumber, out numberAsNumber);
+        numbers.Add(numberAsNumber);
+    }
+    return numbers.ToArray();
+}
+
+int[] myArray = InputArray();
+System.Console.Write("[");
+for (int i = 0; i < myArray.Length; i++)
+{
+    System.Console.Write(myArray[i]);
+    System.Console.Write((i == myArray.Length - 1) ? "" : ", ");
+}
+System.Console.Write("]");
